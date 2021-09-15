4acd46b09da329c0ae4be2b1a6cab1e8eec3d4b6
Michele Ferreira <michele@specsyst.com.br>
2021-09-15 15:10:17 -0300

    Criar função comparar que retorne 1 se x > y, 0 se x == y e -1 se x < y.

---
+++ exercicios/ex5.py
@@ -0,0 +1,14 @@
+x = int(input("Qual o valor de x?"))
+y = int(input("Qual o valor de y?"))
+
+
+def comparar(x, y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    else:
+        return -1
+
+
+print(comparar(x, y))
