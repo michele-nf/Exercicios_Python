1b85b2af7391a169b9192ae7ff3f88076226b888
Michele Ferreira <michele@specsyst.com.br>
2021-09-15 12:00:08 -0300

    Criar função chamada noveLinhas que use função tresLinhas para imprimir nove linhas em branco, e imprimir vinte e sete linhas em branco.

---
+++ exercicios/ex1.py
@@ -0,0 +1,19 @@
+def tresLinhas():
+    print()
+    print()
+    print()
+
+
+def noveLinhas():
+    tresLinhas()
+    tresLinhas()
+    tresLinhas()
+
+
+def vinteSeteLinhas():
+    noveLinhas()
+    noveLinhas()
+    noveLinhas()
+
+
+vinteSeteLinhas()
